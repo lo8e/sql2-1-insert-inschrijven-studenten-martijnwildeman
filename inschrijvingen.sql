a16056a1c248f121e8337225336143f2d784038f
Martijn <1046144@mborijnland.nl>
2019-05-24 12:22:21 +0200

    klassen toegevoegd

---
+++ inschrijvingen.sql
@@ -0,0 +1,6 @@
+USE `lo8e_sql2`;
+
+INSERT INTO Klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES ("lo9e-amo1", "maker", 2019, "amo"),
+		("lo9e-amo2", "mvgi", 2019, "amo"),
+        ("lo9e-amo3", "mbra", 2019, "amo");
