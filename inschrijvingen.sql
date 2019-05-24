ddfa149bb28fb27a4809f160a6917303ba499bba
Martijn <1046144@mborijnland.nl>
2019-05-24 12:33:24 +0200

    studenten toegevoegd

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -1,6 +1,14 @@
 USE `lo8e_sql2`;
 
-INSERT INTO Klassen (klas_code, slb_code, cohort, opleiding_code)
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
 VALUES ("lo9e-amo1", "maker", 2019, "amo"),
 		("lo9e-amo2", "mvgi", 2019, "amo"),
         ("lo9e-amo3", "mbra", 2019, "amo");
+        
+INSERT INTO studenten (ov_nummer, voornaam, achternaam, plaats)
+VALUES (90002, "Martijn", "Wildeman", "Voorhout"),
+(90003, "Erik", "Buenrostro", "Voorschoten"),
+(90004, "Shehr", "Mokiem", "Den Haag"),
+(90005, "Luuk", "Skywalker", "Zoeterwoude");
+
+
