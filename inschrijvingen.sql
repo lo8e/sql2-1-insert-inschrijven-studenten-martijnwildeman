c3234006bd2a85f721afd60c9ff06d67a974816f
Martijn <1046144@mborijnland.nl>
2019-06-03 21:43:21 +0200

    overzicht inschrijvingen

--- inschrijvingen.sql
+++ inschrijvingen.sql
@@ -25,3 +25,10 @@ UPDATE studenten
 SET actief = 0
 WHERE inschrijvings_datum < '2017-01-01';
 
+SELECT studenten.ov_nummer, CONCAT(studenten.voornaam, ' ', studenten.tussenvoegsel, ' ', studenten.achternaam) AS VolledigeNaamStudent, klassen_studenten.klas_code, klassen.cohort, CONCAT(docenten.voorletters, ' ', docenten.tussenvoegsel, ' ', docenten.achternaam) AS VolledigeNaamDocenten
+FROM studenten
+INNER JOIN klassen_studenten ON studenten.ov_nummer = klassen_studenten.ov_nummer
+INNER JOIN klassen ON klassen_studenten.klas_code = klassen.klas_code
+INNER JOIN docenten ON klassen.slb_code = docenten.docent_code
+ORDER BY klas_code, studenten.achternaam ASC;
+
